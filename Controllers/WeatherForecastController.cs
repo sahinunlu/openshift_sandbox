08047c13ef26c41cd51185aeb2ad1879905c1afc
Şahin ÜNLÜ <sahinnunlu@gmail.com>
2022-12-19 13:44:18 +0300

    test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezingg", "Bracingg", "Chillyy", "Cooll", "Mildd", "Warmm", "Balmyy", "Hott", "Swelteringg", "Scorchingg"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
